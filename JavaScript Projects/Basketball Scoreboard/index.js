1101b6ff0e44507f85a0ae9e3f0041914096c2d0
DouglasBaldwin10 <89414146+DouglasBaldwin10@users.noreply.github.com>
2025-04-30 23:09:50 -0400

    Add Basketball Scoreboard Project

---
+++ JavaScript Projects/Basketball Scoreboard/index.js
@@ -0,0 +1,36 @@
+let homescore = 0;
+let guestscore = 0;
+
+function onehome() {
+    homescore += 1;
+    document.getElementById("homescore").textContent = homescore;
+}
+
+function twohome() {
+    homescore += 2;
+    document.getElementById("homescore").textContent = homescore;
+}
+
+function threehome() {
+    homescore += 3;
+    document.getElementById("homescore").textContent = homescore;
+}
+
+function oneguest() {
+    guestscore += 1;
+    document.getElementById("guestscore").textContent = guestscore;
+}
+
+function twoguest() {
+    guestscore += 2;
+    document.getElementById("guestscore").textContent = guestscore;
+}
+
+function threeguest() {
+    guestscore += 3;
+    document.getElementById("guestscore").textContent = guestscore;
+}
+
+function homeclear() {
+    homescore = 0
+    document.getElementById("homescore").
